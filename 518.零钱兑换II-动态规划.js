c0afcfe519135c844970ccc8206264bcc9d017e8
js0205 <467353069@qq.com>
2022-12-04 13:54:06 +0800

    dp

---
+++ 518.零钱兑换II-动态规划.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+    const dp = new Array(amount + 1).fill(0);
+    dp[0] = 1;
+    for (const coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin];
+        }
+    }
+    return dp[amount];
+}
+
+//时间复杂度：O(amount*n)
+//空间复杂度：O(amount)
